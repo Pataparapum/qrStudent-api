4cc5e8c3bc5f2e010eb40a35f490b10693ddd875
cristopher <cristophercc67@gmail.com>
2024-11-27 22:14:57 -0300

    ruta vacia

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,20 @@ import { PrismaService } from './prisma/prisma.service';
 import { SalasModule } from './salas/salas.module';
 import { AlumnosModule } from './alumnos/alumnos.module';
 import { AsistenciaModule } from './asistencia/asistencia.module';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
-  imports: [UsuariosModule, SalasModule, AlumnosModule, AsistenciaModule],
+  imports: [UsuariosModule,
+            SalasModule,
+            AlumnosModule,
+            AsistenciaModule,
+            RouterModule.register([
+              {
+                path: '/',
+                module: UsuariosModule,
+              }
+            ])
+          ],
   providers: [PrismaService],
 })
 export class AppModule {}
